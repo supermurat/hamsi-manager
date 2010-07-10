a375f8bc61c4720c17884ba9cd943c617770e27d
mopened <mopened@4329ce0c-edb2-4c8f-9300-c4d1a0668d28>
2010-07-10 15:27:10 +0000

    Release 0.8.63

---
+++ tags/0.8.63/Source/MyConfigure.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import InputOutputs
+import Universals
+import Settings
+
+class MyConfigure:
+    global reConfigureFile, installKDE4Language, installKDE4Languages
+    
+    def reConfigureFile(_filePath, _installationDirectory=os.path.dirname(Universals.sourcePath)):
+        fileContent = InputOutputs.readFromFile(_filePath).replace("~InstallationDirectory~", _installationDirectory)
+        InputOutputs.writeToFile(_filePath, fileContent)
+            
+    def installKDE4Languages():
+        if Settings.isAvailablePyKDE4():
+            for langCode in InputOutputs.getInstalledLanguagesCodes():
+                installKDE4Language(langCode)
+            Universals.setMySetting("isInstalledKDE4Language", True)
+            return True
+        return False
+            
+    def installKDE4Language(_language="tr_TR", _KDELocalateDir = Universals.getKDE4HomePath() +"share/locale/~langCode~/LC_MESSAGES/"):
+        _KDELocalateDir = str(_KDELocalateDir)
+        if Settings.isAvailablePyKDE4():
+            _KDELocalateDir = _KDELocalateDir.replace("~langCode~", str(_language[:2]))
+            langFile = Universals.sourcePath+"/Languages/" + str(_language)+".mo"
+            if InputOutputs.isFile(_KDELocalateDir+u"HamsiManager.mo")==False:
+                if InputOutputs.isFile(langFile):
+                    if InputOutputs.isDir(_KDELocalateDir)==False:
+                        InputOutputs.makeDirs(_KDELocalateDir)
+                    InputOutputs.copyFileOrDir(langFile,_KDELocalateDir+u"HamsiManager.mo")
+            return True
+        return False
+        
+        
+        
