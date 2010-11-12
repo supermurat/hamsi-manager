eb4b632de69341ff7432fa96ac54dbff664401cd
mopened <mopened@4329ce0c-edb2-4c8f-9300-c4d1a0668d28>
2010-11-12 18:55:51 +0000

    Release 0.9.50

---
+++ tags/0.9.50/Reconfigure.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+## This file is part of HamsiManager.
+## 
+## Copyright (c) 2010 Murat Demir <mopened@gmail.com>      
+##
+## Hamsi Manager is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## Hamsi Manager is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with HamsiManager; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import sys
+import os
+if float(sys.version[:3])<3.0:
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+if str(sys.path[0])=="":
+    sys.path.insert(0, sys.path[1])
+sys.path.insert(1,sys.path[0]+"/Core")
+import Variables
+Variables.checkStartupVariables()
+import Universals
+import RoutineChecks
+if RoutineChecks.checkQt4Exist():
+    import Settings
+    Universals.fillMySettings(False, False, False)
+    Universals.isActivePyKDE4 = False
+    from MyObjects import *
+    import InputOutputs
+    import Dialogs
+    import Execute
+    defaultLangCode = str(MLocale().name())
+    HamsiManagerApp = MApplication(sys.argv)
+    MDir.setSearchPaths("Images", MStringList((Variables.HamsiManagerDirectory+"/Themes/Default/Images/").decode("utf-8")))
+    StyleFile = open(Variables.HamsiManagerDirectory+"/Themes/Default/Style.qss") 
+    HamsiManagerApp.setStyleSheet(StyleFile.read())
+    languageFile = MTranslator()
+    if InputOutputs.isFile(Variables.HamsiManagerDirectory+"/Languages/" + str("HamsiManagerWithQt_"+defaultLangCode+".qm")):
+            languageFile.load((Variables.HamsiManagerDirectory+"/Languages/" + str("HamsiManagerWithQt_"+defaultLangCode+".qm")).decode("utf-8"))
+    elif InputOutputs.isFile(Variables.HamsiManagerDirectory+"/Languages/" + str("HamsiManager_"+defaultLangCode+".qm")):
+            languageFile.load((Variables.HamsiManagerDirectory+"/Languages/" + str("HamsiManager_"+defaultLangCode+".qm")).decode("utf-8"))
+    HamsiManagerApp.installTranslator(languageFile)
+    HamsiManagerApp.setWindowIcon(MIcon("Images:HamsiManager-128x128.png"))
+    HamsiManagerApp.setApplicationName("ConfigureHamsiManager")
+    HamsiManagerApp.setApplicationVersion(Variables.version)
+    HamsiManagerApp.setOrganizationDomain("hamsiapps.com")
+    HamsiManagerApp.setOrganizationName("Hamsi Apps")
+    activePageNo = 0
+    isOnlyRoot = False
+    if len(sys.argv)>1:
+        if sys.argv[1]=="--configurePage":
+            activePageNo = 2
+        if sys.argv[1]=="--pluginPage":
+            activePageNo = 3
+        for argv in sys.argv:
+            if argv=="--onlyRoot":
+                isOnlyRoot = True
+    import MyConfigure
+    class Main(MWidget):
+        def __init__(self, parent=None):
+            MWidget.__init__(self, parent)
+            Universals.MainWindow = self
+            self.isInstallFinised = False
+            self.pageNo, self.pageSize = activePageNo, 4
+            self.vblMain = MVBoxLayout()
+            self.hblMain = MHBoxLayout()
+            self.lblLeftImage = MLabel()
+            self.pmapLeftImage = MPixmap("Images:HamsiManager-128x176.png")
+            self.lblLeftImage.setPixmap(self.pmapLeftImage)
+            self.vblLeftColumn = MVBoxLayout()
+            self.vblLeftColumn.addStretch(1)
+            self.vblLeftColumn.addWidget(self.lblLeftImage)
+            self.vblLeftColumn.addStretch(5)
+            self.hblMain.addLayout(self.vblLeftColumn)
+            self.pages = []
+            for pageNo in range(self.pageSize):
+                self.pages.append(self.createPage(pageNo))
+                if pageNo!=self.pageNo:
+                    self.pages[-1].setVisible(False)
+                self.hblMain.addWidget(self.pages[-1])
+            self.vblMain.addLayout(self.hblMain, 20)
+            self.hblButtons = MHBoxLayout()
+            self.buttons = [MPushButton(MApplication.translate("Reconfigure", "Back")), 
+                            MPushButton(MApplication.translate("Reconfigure", "Forward")), 
+                            MPushButton(MApplication.translate("Reconfigure", "Reconfigure"))]
+            self.hblButtons.addStretch(5)
+            for btnNo, btn in enumerate(self.buttons):
+                if btnNo==len(self.buttons)-1 or btnNo==0:
+                    btn.setVisible(False)
+                self.hblButtons.addWidget(btn, 1)
+                self.connect(btn,SIGNAL("clicked()"),self.pageChanged)
+            self.pbtnCancel = MPushButton(MApplication.translate("Reconfigure", "Cancel"))
+            self.pbtnFinish = MPushButton(MApplication.translate("Reconfigure", "Finish"))
+            self.pbtnFinish.setVisible(False)
+            self.hblButtons.addWidget(self.pbtnCancel, 1)
+            self.hblButtons.addWidget(self.pbtnFinish, 1)
+            self.connect(self.pbtnCancel,SIGNAL("clicked()"),self.close)
+            self.connect(self.pbtnFinish,SIGNAL("clicked()"),self.close)
+            self.vblMain.addLayout(self.hblButtons)
+            self.setLayout(self.vblMain)
+            self.pageChanged(True)
+        
+        def createPage(self, _pageNo):
+            pnlPage = MWidget()
+            HBox = MHBoxLayout()
+            pnlPage.setLayout(HBox)
+            if _pageNo==0:
+                try:f = open(Variables.HamsiManagerDirectory+"/Languages/About_"+defaultLangCode)
+                except:f = open(Variables.HamsiManagerDirectory+"/Languages/About_en_GB")
+                lblAbout = MLabel(f.read().decode("utf-8"))
+                lblAbout.setWordWrap(True)
+                HBox.addWidget(lblAbout)
+            elif _pageNo==1:
+                try:f = open(Variables.HamsiManagerDirectory+"/Languages/License_"+defaultLangCode)
+                except:f = open(Variables.HamsiManagerDirectory+"/Languages/License_en_GB")
+                teCopying = MTextEdit()
+                teCopying.setPlainText(f.read().decode("utf-8"))
+                HBox.addWidget(teCopying)
+            elif _pageNo==2:
+                VBox = MVBoxLayout()
+                VBox.addStretch(10)
+                self.isCreateDesktopShortcut = None
+                self.isCreateExecutableLink = None
+                if Variables.isRunningAsRoot():
+                    self.isCreateExecutableLink = MCheckBox(MApplication.translate("Reconfigure", "Add To The System"))
+                    self.isCreateExecutableLink.setCheckState(Mt.Checked)
+                    lblExecutableLink = MLabel(MApplication.translate("Reconfigure", "Executable Link Path : "))
+                    self.leExecutableLink = MLineEdit(Settings.getUniversalSetting("pathOfExecutableHamsi", "/usr/bin/hamsi").decode("utf-8"))
+                    self.connect(self.isCreateExecutableLink, SIGNAL("stateChanged(int)"),self.createExecutableLinkChanged)
+                    VBox.addWidget(self.isCreateExecutableLink)
+                    HBox1 = MHBoxLayout()
+                    HBox1.addWidget(lblExecutableLink)
+                    HBox1.addWidget(self.leExecutableLink, 10)
+                    VBox.addLayout(HBox1)
+                else:
+                    self.isCreateDesktopShortcut = MCheckBox(MApplication.translate("Reconfigure", "Create Desktop Shortcut."))
+                    self.isCreateDesktopShortcut.setCheckState(Mt.Checked)
+                    VBox.addWidget(self.isCreateDesktopShortcut)
+                VBox.addStretch(10)
+                HBox.addLayout(VBox)
+            elif _pageNo==3:
+                import MyPlugins
+                VBox = MVBoxLayout()
+                VBox.addStretch(10)
+                wPlugins = MyPlugins.MyPluginsForSystem(self)
+                HBox.addWidget(wPlugins)
+                VBox.addStretch(10)
+                HBox.addLayout(VBox)
+            return pnlPage
+        
+        def createExecutableLinkChanged(self, _value):
+            if _value==0:
+                self.leExecutableLink.setEnabled(False)
+            else:
+                self.leExecutableLink.setEnabled(True)
+            
+        def pageChanged(self, _isRunningManual=False):
+            if _isRunningManual==False:
+                senderObject = self.sender()
+                if senderObject==self.buttons[1]:
+                    self.pageNo+=1
+                elif senderObject==self.buttons[0]:
+                    self.pageNo-=1
+                elif senderObject==self.buttons[2]:
+                    self.pageNo+=1
+            for pageNo, pnlPage in enumerate(self.pages):
+                if pageNo!=self.pageNo:
+                    pnlPage.setVisible(False)
+                else:
+                    pnlPage.setVisible(True)
+            self.buttons[0].setVisible(False)
+            self.buttons[1].setVisible(False)
+            self.buttons[2].setVisible(False)
+            self.buttons[1].setText(MApplication.translate("Reconfigure", "Forward"))
+            if self.pageNo==0:
+                self.buttons[1].setVisible(True)
+            elif self.pageNo==1:
+                self.buttons[1].setVisible(True)
+                self.buttons[1].setText(MApplication.translate("Reconfigure", "Accept"))
+            elif self.pageNo==2:
+                self.buttons[0].setVisible(False)
+                self.buttons[1].setVisible(False)
+                self.buttons[2].setVisible(True)
+                self.pbtnCancel.setVisible(True)
+            elif self.pageNo==3:
+                self.buttons[0].setVisible(False)
+                self.buttons[1].setVisible(False)
+                self.buttons[2].setVisible(False)
+                self.pbtnCancel.setVisible(False)
+                self.pbtnFinish.setVisible(True)
+                self.isInstallFinised = True
+            if _isRunningManual==False:
+                if senderObject==self.buttons[2]:
+                    self.reConfigure()
+            
+        def reConfigure(self):
+            oldPathOfExecutableHamsi = Settings.getUniversalSetting("pathOfExecutableHamsi", "/usr/bin/hamsi").decode("utf-8")
+            if InputOutputs.isFile(Variables.HamsiManagerDirectory + "/HamsiManager.desktop"):
+                MyConfigure.reConfigureFile(Variables.HamsiManagerDirectory + "/HamsiManager.desktop", Variables.HamsiManagerDirectory)
+            if self.isCreateDesktopShortcut!=None:
+                if self.isCreateDesktopShortcut.checkState()==Mt.Checked:
+                    import Settings
+                    desktopPath = Variables.getUserDesktopPath()
+                    fileContent = MyConfigure.getConfiguredDesktopFileContent(Variables.HamsiManagerDirectory)
+                    InputOutputs.writeToFile(desktopPath + "/HamsiManager.desktop", fileContent)
+            executableLink = str(self.leExecutableLink)
+            if self.isCreateExecutableLink!=None:
+                if self.isCreateExecutableLink.checkState()==Mt.Checked:
+                    if executableLink.strip()!="":
+                        InputOutputs.createSymLink(Variables.HamsiManagerDirectory+"/HamsiManager.py", executableLink)
+                        Settings.setUniversalSetting("pathOfExecutableHamsi", executableLink)
+                        if oldPathOfExecutableHamsi!=executableLink:
+                            if InputOutputs.isFile(oldPathOfExecutableHamsi):
+                                answer = Dialogs.ask(MApplication.translate("Reconfigure", "Other Hamsi Manager Was Detected"), 
+                                    str(MApplication.translate("Reconfigure", "Other Hamsi Manager executable file was detected. Are you want to delete old executable file? You can delete this old executable file : \"%s\"")) % (oldPathOfExecutableHamsi))
+                                if answer!=Dialogs.Yes:
+                                    InputOutputs.removeFile(oldPathOfExecutableHamsi)
+                    if InputOutputs.isDir("/usr/share/applications/"):
+                        fileContent = MyConfigure.getConfiguredDesktopFileContent(Variables.HamsiManagerDirectory)
+                        InputOutputs.writeToFile("/usr/share/applications/HamsiManager.desktop", fileContent)
+            if Variables.isRunningAsRoot()==False:
+                if InputOutputs.isDir(Variables.userDirectoryPath + "/.local/applications/")==False:
+                    InputOutputs.makeDirs(Variables.userDirectoryPath + "/.local/applications/")
+                fileContent = MyConfigure.getConfiguredDesktopFileContent(Variables.HamsiManagerDirectory)
+                InputOutputs.writeToFile(Variables.userDirectoryPath + "/.local/applications/HamsiManager.desktop", fileContent)
+            MyConfigure.installKDE4Languages()
+            self.isInstallFinised = True
+            
+        def closeEvent(self, _event):
+            if self.isInstallFinised==False:
+                answer = Dialogs.ask(MApplication.translate("Reconfigure", "Finalizing Configuration"), 
+                            MApplication.translate("Reconfigure", "Are You Sure You Want To Quit?"))
+                if answer!=Dialogs.Yes:
+                    _event.ignore()
+            
+    if Variables.isRunningAsRoot()==False and Variables.isRunableAsRoot():
+        if isOnlyRoot:
+            answer = Dialogs.askSpecial(MApplication.translate("Reconfigure", "Are You Want To Run As Root?"), MApplication.translate("Reconfigure", "Hamsi Manager Configure Tool is running with user privileges.<br>Do you want to run Hamsi Manager Configure Tool with root rights?<br>"), MApplication.translate("Reconfigure", "Yes"), MApplication.translate("Reconfigure", "No (Close)"), None)
+        else:
+            answer = Dialogs.askSpecial(MApplication.translate("Reconfigure", "Are You Want To Run As Root?"), MApplication.translate("Reconfigure", "Hamsi Manager Configure Tool is running with user privileges.<br>Do you want to run Hamsi Manager Configure Tool with root rights?<br>"), MApplication.translate("Reconfigure", "Yes"), MApplication.translate("Reconfigure", "No (Continue as is)"), None)
+        if answer==MApplication.translate("Reconfigure", "Yes"):
+            myParametres = []
+            if len(sys.argv)>1:
+                for x in sys.argv[1:]:
+                    myParametres.append(x)
+            NewApp = Execute.executeReconfigureAsRoot(myParametres)
+            sys.exit()
+        elif isOnlyRoot:
+            sys.exit()
+    MainWidget=Main()
+    MainWidget.setWindowTitle(MApplication.translate("Reconfigure", "Hamsi Manager Configure Tool") + " " + MApplication.applicationVersion())
+    MainWidget.setGeometry(300, 300, 650, 350)
+    MainWidget.show()
+    Universals.isStartingSuccessfully = True
+    sys.exit(HamsiManagerApp.exec_())
+else:
+    sys.exit()
+    
+    
+        
+    
---
+++ tags/0.9.50/Taggers/EyeD3Tagger.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+## This file is part of HamsiManager.
+## 
+## Copyright (c) 2010 Murat Demir <mopened@gmail.com>      
+##
+## Hamsi Manager is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## Hamsi Manager is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with HamsiManager; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+isAvailable = False
+try:
+    import eyeD3
+    isAvailable = True
+except:pass
+import Taggers
+from MyObjects import *
+import InputOutputs
+pluginName = "eyeD3"
+
+class Tagger():
+    global getImageTypes, getImageTypesNo, getTaggerTypes, getTaggerTypesName, getAvailableLabelsForTable, getAvailableKeysForTable
+    
+    def __init__(self):
+        self.filePath = None
+        self.tag = None
+        self.Mp3AudioFile = None
+        
+    def loadFile(self, _filePath):
+        self.filePath = _filePath
+        try:
+            self.tag = eyeD3.Tag()
+            self.tag.link(self.filePath.encode(InputOutputs.fileSystemEncoding), Taggers.getSelectedTaggerType())
+        except:
+            self.tag = eyeD3.Tag()
+            self.tag.link(self.filePath, Taggers.getSelectedTaggerType())
+        try:
+            self.Mp3AudioFile = eyeD3.Mp3AudioFile(self.filePath.encode(InputOutputs.fileSystemEncoding))
+        except:
+            try:
+                self.Mp3AudioFile = eyeD3.Mp3AudioFile(self.filePath)
+            except:
+                self.Mp3AudioFile = None
+                
+    def loadFileForWrite(self, _filePath):
+        self.filePath = _filePath
+        try:
+            self.tag = eyeD3.Tag()
+            self.tag.link(self.filePath.encode(InputOutputs.fileSystemEncoding), Taggers.getSelectedTaggerType())
+        except:
+            self.tag = eyeD3.Tag()
+            self.tag.link(self.filePath, Taggers.getSelectedTaggerType())
+        self.Mp3AudioFile = None
+        self.correctForMusicTagType()
+        
+    def update(self):
+        self.tag.update()
+        
+    def isAvailableFile(self):
+        return eyeD3.isMp3File(self.filePath)
+        
+    def getValuesForMusicTagType(self, _value):
+        if Taggers.getSelectedTaggerType()==eyeD3.ID3_V1:
+            return unicode(_value).encode("latin1")
+        else:
+            return _value
+        
+    def correctValuesForMusicGenre(self, _genre):
+        import re
+        regex = re.compile("^[A-Z 0-9+/\-\|!&'\.]+\00*$", re.IGNORECASE)
+        genreStrAmended = ""
+        for x in [y for x, y in enumerate(_genre) if regex.match(y)]:
+            genreStrAmended+=x
+        return str(genreStrAmended)
+            
+    def correctValuesForMusicTagType(self, _value):
+        if Taggers.getSelectedTaggerType()==eyeD3.ID3_V1:
+            return unicode(str(_value), "latin1")
+        else:
+            return unicode(str(_value))
+    
+    def correctForMusicTagType(self):
+        self.tag.setVersion(Taggers.getSelectedTaggerType())
+        if Taggers.getSelectedTaggerType()==eyeD3.ID3_V2:
+            self.tag.setTextEncoding(eyeD3.frames.UTF_8_ENCODING)
+        
+    def getArtist(self):
+        try:return self.getValuesForMusicTagType(str(self.tag.getArtist()))
+        except:return "None"
+        
+    def getTitle(self):
+        try:return self.getValuesForMusicTagType(str(self.tag.getTitle()))
+        except:return "None"
+        
+    def getAlbum(self):
+        try:return self.getValuesForMusicTagType(str(self.tag.getAlbum()))
+        except:return "None"
+        
+    def getTrackNum(self):
+        try:
+            if Taggers.getSelectedTaggerType()==eyeD3.ID3_V2:
+                return str(str(self.tag.getTrackNum()[0])+"/"+str(self.tag.getTrackNum()[1]))
+            else:
+                return str(self.tag.getTrackNum()[0])
+        except:return "None"
+        
+    def getYear(self):
+        try:return str(self.tag.getYear())
+        except:return "None"
+        
+    def getGenre(self):
+        try:return self.getValuesForMusicTagType(str(self.tag.getGenre()))
+        except:return "None"
+        
+    def getFirstComment(self):
+        try:return self.getValuesForMusicTagType(str(self.tag.getComment()))
+        except:return "None"
+        
+    def getFirstLyrics(self):
+        try:
+            if len(self.tag.getLyrics())>0:
+                return self.getValuesForMusicTagType(str(self.tag.getLyrics()[0].lyrics))
+            else:
+                return "None"
+        except:return "None"
+        
+    def getImages(self):
+        try:
+            images = []
+            imageTypes = getImageTypes()
+            imageTypesNo = getImageTypesNo()
+            for image_no, image in enumerate(self.tag.getImages()):
+                images.append([])
+                for no,type in enumerate(imageTypes):
+                    if str(image.pictureType)==imageTypesNo[no]:
+                        images[image_no].append(no)
+                        images[image_no].append(type)
+                        break
+                images[image_no].append(image.mimeType)
+                images[image_no].append(image.imageData)
+            return images
+        except:
+            return []
+        
+    def setArtist(self, _value):
+        self.tag.getArtist(self.correctValuesForMusicTagType(_value))
+        
+    def setTitle(self, _value):
+        self.tag.setTitle(self.correctValuesForMusicTagType(_value))
+        
+    def setAlbum(self, _value):
+        self.tag.setAlbum(self.correctValuesForMusicTagType(_value))
+        
+    def setTrackNum(self, _value, _numberOfTracks):
+        track = []
+        track_temp=_value
+        if track_temp.find("/")!=-1:
+            track_temp2 = track_temp.split("/")
+            try:    track.append(int(track_temp2[0]))
+            except: track.append(None)
+            try:    track.append(int(track_temp2[1]))
+            except: track.append(_numberOfTracks)
+        elif track_temp=="":
+            track.append(None)
+            track.append(None)
+        else:
+            try:    track.append(int(track_temp))
+            except: track.append(None)    
+            track.append(_numberOfTracks)
+        if Taggers.getSelectedTaggerType()==eyeD3.ID3_V2:
+            self.tag.setTrackNum(track)
+        else:
+            try:self.tag.setTrackNum(track.split("/")[0])
+            except:
+                try:self.tag.setTrackNum(track[0])
+                except:self.tag.setTrackNum(track)
+                    
+    def setDate(self, _value):
+        if len(_value)==4:
+            self.tag.setDate(_value)
+        elif _value=="":
+            self.tag.setDate(None)
+        else:
+            from time import gmtime
+            self.tag.setDate(gmtime()[0])
+        
+    def setGenre(self, _value):
+        self.tag.setGenre(self.correctValuesForMusicGenre(self.correctValuesForMusicTagType(_value)))
+        
+    def setFirstComment(self, _value):
+        self.tag.removeComments()
+        self.tag.addComment(self.correctValuesForMusicTagType(_value))
+        
+    def setFirstLyrics(self, _value):
+        self.tag.removeLyrics()
+        self.tag.addLyrics(self.correctValuesForMusicTagType(_value))
+        
+    def addImage(self, _ImageType,_ImagePath):
+        if Taggers.getSelectedTaggerType()==eyeD3.ID3_V2:
+            self.tag.addImage(_ImageType,_ImagePath)
+        
+    def getSize(self):
+        if self.Mp3AudioFile!=None:
+            try:return str(self.Mp3AudioFile.getSize())
+            except:return "None"
+        return "None"
+        
+    def getPlayTimeString(self):
+        if self.Mp3AudioFile!=None:
+            try:return str(self.Mp3AudioFile.getPlayTimeString())
+            except:return "None"
+        return "None"
+        
+    def getSampleFreq(self):
+        if self.Mp3AudioFile!=None:
+            try:return str(self.Mp3AudioFile.getSampleFreq())
+            except:return "None"
+        return "None"
+        
+    def getBitRateString(self):
+        if self.Mp3AudioFile!=None:
+            try:return str(self.Mp3AudioFile.getBitRateString())
+            except:return "None"
+        return "None"
+        
+    def getImageTypes():
+        return ["Other (Default)","Icon","Other Icon","Front Cover","Back Cover","Leaflet","Media",
+            "Lead Artist","Artist","Leader","Band","Composer","Lyrics By","Recorded At",
+            "Recording","Performing","Video","Made Famous","Example","Band Logo","Publisher Logo"]
+            
+    def getImageTypesNo():
+        return ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","10","11","12","13","14"] 
+        
+    def getTaggerTypes():
+        return [eyeD3.ID3_V2, eyeD3.ID3_V1]
+        
+    def getTaggerTypesName():
+        return ["ID3 V2", "ID3 V1"]
+        
+    def getAvailableKeysForTable():
+        keys = ["Directory", "File Name", "Artist", "Title", "Album", 
+                              "Track No", "Year", "Genre", "Comment", "Lyrics"]
+        if Taggers.getSelectedTaggerType()!=getTaggerTypes()[0]:
+            t = keys.pop()
+        return keys
+        
+    def getAvailableLabelsForTable():
+        labels = [translate("MusicTable", "Directory"), 
+            translate("MusicTable", "File Name"), 
+            translate("MusicTable", "Artist"), 
+            translate("MusicTable", "Title"), 
+            translate("MusicTable", "Album"), 
+            translate("MusicTable", "Track No"), 
+            translate("MusicTable", "Year"), 
+            translate("MusicTable", "Genre"), 
+            translate("MusicTable", "Comment"), 
+            translate("MusicTable", "Lyrics")]
+        if Taggers.getSelectedTaggerType()!=getTaggerTypes()[0]:
+            t = labels.pop()
+        return labels
+        
+        
+        
+        
+        
