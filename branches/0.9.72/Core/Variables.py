91cc2dced55488d490dc1f169ed68cbb3dcc01a5
mopened <mopened@4329ce0c-edb2-4c8f-9300-c4d1a0668d28>
2011-05-30 17:57:29 +0000

    Released 0.9.72.

--- branches/0.9.72/Core/Variables.py
+++ branches/0.9.72/Core/Variables.py
@@ -28,8 +28,8 @@ class Variables():
     osName = os.name
     isPython3k = float(sys.version[:3])>=3.0
     Catalog = "HamsiManager" 
-    version = "0.9.71"
-    intversion = 971
+    version = "0.9.72"
+    intversion = 972
     settingVersion = "970"
     aboutOfHamsiManager = ""
     HamsiManagerDirectory = sys.path[0]
