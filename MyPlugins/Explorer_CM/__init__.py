6752add25e2f409d70c1e62ab88dfdd26efa54f7
mopened <mopened@4329ce0c-edb2-4c8f-9300-c4d1a0668d28>
2012-03-17 16:22:39 +0000

    Fixed a bug.

--- MyPlugins/Explorer_CM/__init__.py
+++ MyPlugins/Explorer_CM/__init__.py
@@ -16,7 +16,7 @@
 ## along with HamsiManager; if not, write to the Free Software
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-import os
+import os, sys
 from Core import Variables
 from Core import Universals
 from Core import Dialogs
